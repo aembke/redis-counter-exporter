ad39b381bb475db899cc55d3ebaa6926bf46377f
Alec Embke <aembke@gmail.com>
2024-11-05 12:55:48 -0800

    chore: build script

---
+++ tests/scripts/build_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+declare -a targets=(
+  "aarch64-unknown-linux-gnu"
+  "aarch64-unknown-linux-musl"
+  "x86_64-unknown-linux-gnu"
+  "x86_64-unknown-linux-musl"
+)
+
+for target in "${targets[@]}"
+do
+  Echo "Building for $target"
+  cross build --release --bin redis-counter-exporter --target $target
+done
+
+mkdir tests/tmp/releases
+for target in "${targets[@]}"
+do
+  cp "target/$target/release/redis-counter-exporter" "tests/tmp/releases/redis-counter-exporter-$target"
+done
+
+echo "Finished building."
